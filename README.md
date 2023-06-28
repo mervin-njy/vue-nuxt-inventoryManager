263f5cb7450046007248c5d5ad625fdd858f2d5c
mervin-njy <mervin.njy@outlook.com>
2023-06-28 19:57:18 +0800

    created project + installed dependencies (tailwind CSS + daisy UI)

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # vue-nuxt-inventoryManager
 Simple CRUD app where user can view a list of apartments, each with inventory management features.
+
+### Dependencies
+npm install --save-dev @nuxtjs/tailwindcss
+npm i daisyui
---
+++ inventory-app/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+
+  <div class="flex justify-center h-screen space-x-4 items-center">
+    <div class="card card-compact w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img src="https://i.imgur.com/EQv76zR.jpg" alt="dog in alley" />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">Pup in the alley</h2>
+        <p class="mb-4">If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">Approve</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
