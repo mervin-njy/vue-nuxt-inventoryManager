8f79c9551ea44d6ca3fc446f7baee2507adc8a85
mervin-njy <mervin.njy@outlook.com>
2023-06-29 16:19:59 +0800

    created directories + pages/apartment/index.vue home page to fetch and display apartment list from mock-db directory (only TODO steps)

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # vue-nuxt-inventoryManager
+
 Simple CRUD app where user can view a list of apartments, each with inventory management features.
 
 ### Dependencies
+
 npm install --save-dev @nuxtjs/tailwindcss
 npm i daisyui
---
+++ inventory-app/pages/apartment/index.vue
@@ -0,0 +1,9 @@
+// NOTE: vue's default base route / home page, i.e., not /index route
+
+// TODO: 
+
+// 1. fetch list of apartments from server / mock-db via simluated API w/ asyncData method
+
+// 2. Display apartment details {Address, Floor, Door Number}
+
+// 3. (STRETCH GOAL) Search function to filter
