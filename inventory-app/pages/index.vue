5f84e2a823b483b86d92bbf5f13847607448d333
mervin-njy <mervin.njy@outlook.com>
2023-06-29 18:18:14 +0800

    added reroute from home to apartments page for the purpose of this project

--- inventory-app/pages/index.vue
+++ inventory-app/pages/index.vue
@@ -1,12 +1,20 @@
-// NOTE: vue's default base route / home page
+<!-- NOTE: vue's default base route / home page -->
+<!-- Added redirect (@nuxt.config.ts) from "/" to "/apartments" for this project -->
+<!-- Only notes here -->
+
 <template>
   <div>
     <header>
-        <nav>
-            <ul>
-                <li></li>
-            </ul>
-        </nav>
+      <NuxtLink to="/">SiteTitle</NuxtLink>
+      <nav>
+        <!-- why NuxtLink over <a> tags -->
+        <!-- 1. While sending requests to server, Nuxt intercepts and swaps out components depending on page  -->
+        <!-- 2. Automatically set up Active class Links (when clicked) for styling -->
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/apartments">Apartments</NuxtLink></li>
+        </ul>
+      </nav>
     </header>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus voluptas
